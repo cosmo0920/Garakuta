428f25f2fa9f2a560e4ac8159fc1b04e4e75bdac
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-01-26 04:48:31 +0900

    Json4s: refactoring.

--- Json4s_Test/main.scala
+++ Json4s_Test/main.scala
@@ -12,12 +12,20 @@ object Main {
 
   def forecast(req: Future[String]) = {
     val json = parse(req())
+    val JString(time) = json\ "publicTime"
+    val pubTime = "更新時刻: " + time
+    val JString(minTemp) = json\ "forecasts"\ "temperature"\ "min"\"celsius"
+    val JString(maxTemp) = json\ "forecasts"\ "temperature"\ "max"\"celsius"
+    val minmaxtemp = "最高気温: " + maxTemp + "℃ 最低気温: " + minTemp + "℃ です。"
     val JString(location) = json\ "location" \"city"
     val JArray(List(JString(today), JString(tomorrow))) = json\ "forecasts"\ "dateLabel"
     val JArray(List(JString(todayf), JString(tomorrowf))) = json\ "forecasts"\ "telop"
-    val forecast = location + "の" + today + "、" + tomorrow + "の天気は" + todayf + "、" + tomorrowf + "です。"
+    val forecast = "簡易予報: " + location + "の" + today + "、" + tomorrow + "の天気は" + todayf + "、" + tomorrowf + "です。"
     val JString(descr) = json\ "description"\ "text"
+    val description = "詳細: " + descr
+    println(pubTime)
+    println(minmaxtemp)
     println(forecast)
-    println(descr)
+    println(description)
   }
 }
