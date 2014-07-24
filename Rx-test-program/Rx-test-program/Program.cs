15716c01f2e77e584ba49264cd3f47dd3d6b01cd
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-07-25 00:04:29 +0900

    Rx-Using-Test: fir using Rx in C#

---
+++ Rx-test-program/Rx-test-program/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Web;
+using System.Net;
+using System.IO;
+using System.Text;
+
+namespace Rxtestprogram
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create ("http://github.com");
+			var rx = GetDocumentContents (request).ToObservable();
+			rx.Subscribe (Console.Write);
+		}
+
+		private static string GetDocumentContents(System.Net.HttpWebRequest request)
+		{
+			string documentContents;
+			HttpWebResponse response = (HttpWebResponse)request.GetResponse ();
+			using (Stream receiveStream = response.GetResponseStream())
+			{
+				using (StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8))
+				{
+					documentContents = readStream.ReadToEnd();
+				}
+			}
+			return documentContents;
+		}
+	}
+}
