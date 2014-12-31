2897e2a6a254b0bf3ca731b702eb480960d8b97c
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-12-31 22:42:45 +0900

    Coq: Use meaningful theorem names

--- Coq/polymorphic_types_ex6_51.v
+++ Coq/polymorphic_types_ex6_51.v
@@ -1,13 +1,13 @@
 (* Empty Set *)
 Inductive Empty_set : Set :=.
 
-Theorem strange_eq : forall x y:Empty_set, x=y.
+Theorem empty_set_eq : forall x y:Empty_set, x=y.
 Proof.
   intros x y.
   elim x.
 Qed.
 
-Theorem strange_neq : forall x y:Empty_set, ~x=y.
+Theorem empty_set_neq : forall x y:Empty_set, ~x=y.
 Proof.
   intros x y.
   elim x.
