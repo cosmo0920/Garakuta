90f0c8daad0c3b0490036fe35e3917e3d66fec0b
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-04-10 23:32:32 +0900

    Add FSharpScript examples
    * Download internet files

---
+++ FSharpScript/DownloadGroonga.fsx
@@ -0,0 +1,24 @@
+﻿open System
+open System.Net
+
+let webcilent (address :String)(url: String) =
+    let wc = new WebClient()
+    let uri = Uri(address)
+    try
+        wc.DownloadFile(uri, url)
+    with
+    | ex -> printfn "%s" ex.Message |> ignore
+
+let downLoadGroonga (groongaVer: String) =
+    let url = String.Format("http://packages.groonga.org/source/groonga/groonga-{0}.zip", groongaVer)
+    let saveFileName = String.Format("groonga-{0}.zip", groongaVer)
+    try
+        webcilent url saveFileName
+    with
+    | ex -> printfn "%s" ex.Message; ()
+
+let version = "5.0.2"
+
+Console.WriteLine("downloading groonga {0}...", version)
+downLoadGroonga version
+Console.WriteLine "done!"
---
+++ FSharpScript/DownloadMroonga.fsx
@@ -0,0 +1,22 @@
+﻿open System
+open System.Net
+
+let webcilent (address :String)(url: String) =
+    let wc = new WebClient()
+    let uri = Uri(address)
+    try
+        wc.DownloadFile(uri, url)
+    with
+    | ex -> printfn "%s" ex.Message |> ignore
+
+let downLoadMroongaForWindows (mariaDBVer: String) (mroongaVer: String) =
+    let url = String.Format("http://packages.groonga.org/source/mroonga/mariadb-{0}-with-mroonga-{1}-for-windows.zip", mariaDBVer, mroongaVer)
+    let saveFileName = String.Format("mariadb-{0}-with-mroonga-{1}-for-windows.zip", mariaDBVer, mroongaVer)
+    webcilent url saveFileName
+
+let mariaDBVer = "10.0.17"
+let mroongaVer = "5.01"
+
+Console.WriteLine("downloading mariadb-{0}-with-mroonga-{1}-for-windows.zip...", mariaDBVer, mroongaVer)
+downLoadMroongaForWindows mariaDBVer mroongaVer
+Console.WriteLine "done!"
