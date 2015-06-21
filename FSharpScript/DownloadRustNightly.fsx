55852978c26c7ba2f1d9feae05609a595ee61447
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-06-22 08:21:27 +0900

    F# Script: Add Rust 64bit nightly msi download script

---
+++ FSharpScript/DownloadRustNightly.fsx
@@ -0,0 +1,19 @@
+﻿open System
+open System.Net
+
+let webcilent (address :String)(url: String) =
+    let wc = new WebClient()
+    let uri = Uri(address)
+    try
+        wc.DownloadFile(uri, url)
+    with
+    | ex -> printfn "%s" ex.Message |> ignore
+
+let downLoadRustNightlyForWindows =
+    let url = "https://static.rust-lang.org/dist/rust-nightly-x86_64-pc-windows-gnu.msi"
+    let saveFileName = "rust-nightly-x86_64-pc-windows-gnu.msi"
+    webcilent url saveFileName
+
+Console.WriteLine "Downloading Rust nightly..."
+downLoadRustNightlyForWindows
+Console.WriteLine "done!"
