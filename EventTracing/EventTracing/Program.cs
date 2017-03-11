80f5caf7276684d4c7223bd89842f3f5691d7334
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2017-03-11 15:19:12 +0900

    Add ETW(Event Tracing for Windows) example

---
+++ EventTracing/EventTracing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Diagnostics.Tracing;
+
+namespace EventTracing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TailEventSource.Log.Critical("This is a test for windows event log.");
+            Console.WriteLine("Confirm windows eventlog. (Press Enter)");
+            Console.ReadLine();
+        }
+    }
+
+    [EventSource(Name = "TailEventSource")]
+    public sealed class TailEventSource : EventSource
+    {
+        public static readonly TailEventSource Log = new TailEventSource();
+
+        [Event(1, Message = "{0}", Channel = EventChannel.Admin)]
+        public void Critical(string message)
+        {
+            WriteEvent(1, message);
+        }
+    }
+}
