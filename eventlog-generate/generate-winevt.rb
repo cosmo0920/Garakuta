280174c0a6d12b15e38e3c1e4a45cfbf8c2ea4e9
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2017-03-12 17:15:17 +0900

    eventlog-generate: Add Ruby vesion of Windows eventlog geneator

---
+++ eventlog-generate/generate-winevt.rb
@@ -0,0 +1,49 @@
+require 'win32/eventlog'
+
+class EventLog
+  def initialize
+    @logger = Win32::EventLog.new
+    @app_source = "fluent-plugins"
+  end
+
+  def info(event_id, message)
+    puts message
+    @logger.report_event(
+      source: @app_source,
+      event_type: Win32::EventLog::INFO_TYPE,
+      event_id: event_id,
+      data: message
+    )
+  end
+
+  def warn(event_id, message)
+    puts message
+    @logger.report_event(
+      source: @app_source,
+      event_type: Win32::EventLog::WARN_TYPE,
+      event_id: event_id,
+      data: message
+    )
+  end
+
+  def crit(event_id, message)
+    puts message
+    @logger.report_event(
+      source: @app_source,
+      event_type: Win32::EventLog::ERROR_TYPE,
+      event_id: event_id,
+      data: message
+    )
+  end
+
+end
+
+class FluentPluginEvtSvc
+  def run
+    eventlog = EventLog.new()
+    eventlog.info(65500, "Hi, from fluentd-plugins!! at " + Time.now.strftime("%Y/%m/%d %H:%M:%S "))
+  end
+end
+
+svc = FluentPluginEvtSvc.new
+svc.run
